a4b1eda134d63325aefc6befe889f96b9d5f342e
AdrianOlbers <58517433+adrianolbers@users.noreply.github.com>
2021-11-29 11:08:23 +0100

    Update SudokuMain.java

--- Suduko/src/SudukoPack/SudokuMain.java
+++ Suduko/src/SudukoPack/SudokuMain.java
@@ -1,5 +1,8 @@
 package SudukoPack;
 
+/**
+Äkta main där vi endast skapar objektet för att använda alla funktioner och rita ut det.
+*/
 final class SudokuMain {
 
 	public static void main(String[] args) {
